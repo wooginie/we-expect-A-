a6232243cc9cd87593fc703b1c5076439d92345a
wooginie <woojin000612@naver.com>
2023-04-28 14:48:26 +0900

    24시즌예측

---
+++ 24.cpp
@@ -0,0 +1,41 @@
+include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define K_Teams 13
+
+typedef struct teamInformation {
+   char name_of_team[100];
+   char team_24season_ranking[100];
+   char winning_reason[100];
+}INFO;
+
+int main()
+{
+   int i;
+   char* x;
+
+   INFO list_team[K_Teams];
+
+   FILE* fp = fopen("file name ", "r");
+
+    if(fp==NULL){
+        printf("failed");
+        return -1;
+    }
+    char line[1024];
+      for (i = 0; i < K_Teams; i++) {
+         fgets(line, 1024, fp);
+         x = strtok(line, ",");
+         while (x != NULL) {
+            strcpy(list_team[i].name_of_team, x);
+            x = strtok(NULL, ",");
+            strcpy(list_team[i].team_24season_ranking, x);
+            x = strtok(NULL, ",");
+            strcpy(list_team[i].winning_reason, x);
+            x = strtok(NULL, ",");
+         }
+
+      }
+}
+
