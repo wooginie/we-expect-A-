7de5d69fbae843f3d43de7079db7c65e7a361f66
sh9125 <ku001205@naver.com>
2023-04-30 15:54:21 +0900

    팀 정보 출력 코드

---
+++ 팀정보.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define K_leagueTeam 13
+
+typedef struct teamInformation {
+	char team_name[100];
+	char team_info[100];
+	char last_ranking[100];
+	char key_player[100];
+	char team_song[100];
+}INFO;
+
+int main() {
+	int i;
+	char* a;
+
+
+	INFO list_team[K_leagueTeam];
+
+	FILE* fp = fopen("k리그_팀정보.csv", "r");
+
+	if (fp == NULL) {
+		printf("파일 읽기 실패!");
+		return -1;
+	}
+
+	char line[1024];
+	for (i = 0; i < K_leagueTeam; i++) {
+		fgets(line, 1024, fp);
+		a = strtok(line, ",");
+		while (a != NULL) {
+			strcpy(list_team[i].team_name, a);
+			a = strtok(NULL, ",");
+			strcpy(list_team[i].team_info, a);
+			a = strtok(NULL, ",");
+			strcpy(list_team[i].last_ranking, a);
+			a = strtok(NULL, ",");
+			strcpy(list_team[i].key_player, a);
+			a = strtok(NULL, ",");
+			strcpy(list_team[i].team_song, a);
+			a = strtok(NULL, ",");
+		}
+	}
+
+	for (i = 0; i < K_leagueTeam; i++) {
+		printf("%s \n", list_team[i].team_name);
+		printf("팀 소개: %s \n", list_team[i].team_info);
+		printf("저번시즌 순위: %s \n", list_team[i].last_ranking);
+		printf("핵심 선수: %s \n", list_team[i].key_player);
+		printf("응원가: %s \n", list_team[i].team_song);
+		printf("\n");
+	}
+
+	fclose(fp);
+
+	return 0;
+}
