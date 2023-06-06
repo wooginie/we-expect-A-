66b04e7960104843637e02efdf67012823e70a45
wooginie <114981044+wooginie@users.noreply.github.com>
2023-06-06 22:05:22 +0900

    Update 24.cpp
    내년에 있을 예상되는 팀 보는 함수

--- 24.cpp
+++ 24.cpp
@@ -1,8 +1,11 @@
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-#define K_Teams 13
+#define K_Teams 15
 
 typedef struct teamInformation {
    char name_of_team[100];
@@ -10,6 +13,12 @@ typedef struct teamInformation {
    char winning_reason[100];
 }INFO;
 
+void bring_allteams(INFO** team) {
+	printf("내년에 있을 예상되는 팀 보기 \n");
+        for (int i = 0; i < K_Teams; i++) {
+            bring_information(team[i]);
+        }
+}
 int main()
 {
    int i;
